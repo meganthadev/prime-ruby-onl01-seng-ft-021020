b99fcc5ad6e9783e842d9d98f9837fe7e4b87250
Megan Horsfield <meganthadev@gmail.com>
2020-02-22 05:36:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(n)
+
+return false if !n.integer?
+
+return false if n <= 1
+
+return true if n == 2
+
+(2..n-1).each {|i| return false if n % i == 0}
+
+return true
+end
